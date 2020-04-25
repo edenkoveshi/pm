f15e39805cd9e8de5e218f1ecc3144c3d757c49a
edenkoveshi <edenk3012@gmail.com>
2020-04-25 23:52:12 +0300

    Learning angular

--- angular7-app/src/app/pm-table/pm-table.component.ts
+++ angular7-app/src/app/pm-table/pm-table.component.ts
@@ -6,11 +6,15 @@ import db from '../../assets/db.json';
   templateUrl: './pm-table.component.html',
   styleUrls: ['./pm-table.component.css'],
 })
+
 export class PmTableComponent implements OnInit {
   db: any;
   servers: Array<any>; //List of servers that will be DISPLAYED
   keys: string[];
   editField: string;
+  selectedDomain: string;
+  numServersInSelectedDomain: number = 1;
+  statsInDomain: number[] = [0, 0, 0];
 
   constructor() {
     console.log(db)
@@ -18,18 +22,18 @@ export class PmTableComponent implements OnInit {
 
   ngOnInit() {
     this.db = db;
-    this.servers = this.db.servers;
+    this.servers = this.db.Servers;
     //this.filterServers()
     this.sortServers();
     this.keys = Object.keys(this.servers[0]);
   }
 
-  updateList(id: number, property: string, event: any):void {
+  updateList(id: number, property: string, event: any): void {
     const editField = event.target.textContent;
     window.alert(editField)
   }
 
-  changeValue(id: number, property: string, event: any):void {
+  changeValue(id: number, property: string, event: any): void {
     this.editField = event.target.textContent;
   }
 
@@ -37,7 +41,7 @@ export class PmTableComponent implements OnInit {
     this.servers.sort((s1, s2) => s1.App.localeCompare(s2.App));
   }
 
-  getServers(): Array<any>{
+  getServers(): Array<any> {
     return this.servers;
   }
 
@@ -46,15 +50,33 @@ export class PmTableComponent implements OnInit {
     this.sortServers();
   }
 
-  filterServers(filter: (s1) => boolean): void {
-    this.servers = this.db.servers.filter(filter);
+  filterServers(filter: (s1) => boolean): Array<any> {
+    return this.db.Servers.filter(filter);
   }
 
   filterByDomain(domain: string): void {
-    this.filterServers(s => {
+    this.selectedDomain = domain;
+    this.servers = this.filterServers(s => {
       return s.Domain === domain;
     });
+    this.numServersInSelectedDomain = this.servers.length;
+    this.statsInDomain = this.getServerStatistics(domain);
+
   }
 
+  getServerStatistics(domain: string): number[] {
+    let UndeployedServers = this.filterServers(s => s.Domain == domain && !s.PMDeployed);
+    let UnrebootedServers = this.filterServers(s => s.Domain == domain && s.PMDeployed && !s.RebootPerformed);
+    let Finished = this.filterServers(s => s.Domain == domain && s.PMDeployed && s.RebootPerformed);
+    return [UndeployedServers.length, UnrebootedServers.length, Finished.length]
+  }
+
+  getProgressBarStyle(num: number) {
+    let colors: string[] = ["#2AB025", "#EC7423", "#EC2323"];
+    return {
+      'width': (this.statsInDomain[num] / this.numServersInSelectedDomain) * 100 + '%',
+      'background-color': colors[num]
+    }
+  }
 
 }
